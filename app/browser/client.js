e7763c9c55fe26bb1c88b99eed5620d6173d5afd
Philipp <philipp.arnosti@webtiser.ch>
2015-04-28 20:40:20 +0200

    handlebars

--- app/browser/client.js
+++ app/browser/client.js
@@ -3,9 +3,39 @@
 'use strict';
 
 var ipc = require('ipc');
+var fs = require('fs');
+
+var handlebars = require("./resources/bower_components/handlebars/handlebars.js");
+var layouts = require('./resources/bower_components/handlebars-layouts/dist/handlebars-layouts.js');
+ 
+layouts.register(handlebars);
+
+
+var filenames = fs.readdirSync("./app/browser/resources/templates/");
+ 
+filenames.forEach(function (filename) {
+  var matches = /^([^.]+).hbs$/.exec(filename);
+  if (!matches) {
+    return;
+  }
+  var name = matches[1];
+  var template = fs.readFileSync("./app/browser/resources/templates/" + filename, 'utf8');
+  handlebars.registerPartial(name, template);
+});
+
 
 function launch (path) {
     ipc.send("launch",path);
+}
+
+function loadPage(page) {
+
+    var templateCode = fs.readFile("./app/browser/"+ page, {encoding:"utf-8"}, function(err,data){
+        if (err) throw err;
+         var template = handlebars.compile(data);
+        $('html').html(template({"test":"asd"}));
+        $('.username').html("username");
+    });
 }
 
 function chooseGame(e){
